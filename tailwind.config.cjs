ee19b7e4cb76532d5b7e3c87dc4daf01e1680692
virmanbgs-collab <virman.bgs@gmail.com>
2025-10-22 13:19:13 +0700

    Add files via upload

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,10 @@
-
 module.exports = {
-  content: ['./index.html','./src/**/*.{js,jsx,ts,tsx}'],
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       colors: {
-        tpggreen:'#2f855a',
-        tpglight:'#f0fff4'
+        tpggreen: '#2f855a',
+        tpglight: '#f0fff4'
       }
     }
   },
